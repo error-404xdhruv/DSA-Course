6459580a199f5d49d08ef0afffcb384d8d8be87b
Dhruv <dhruv.nitj@gmail.com>
2022-04-16 18:57:14 +0530

    apr16

--- 31_decimal2Binary.cpp
+++ 31_decimal2Binary.cpp
@@ -12,6 +12,5 @@ using namespace std ;
 int main(int argc, char const *argv[])
 {
     
-
     return 0;
 }
---
+++ 32_binary2Decimal.cpp
@@ -0,0 +1,28 @@
+
+// binary to decimal
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+#include <cstring>
+#include <string>
+
+using namespace std ;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Enter some number in the Binary form: ";
+    int num ;
+    cin >> num ;
+    
+    int count = 0, sum = 0;
+    while (num!=0)
+    {
+        sum += (num%10)*pow(2,count);
+        num/=10;
+        count++ ;
+    }
+    cout << sum << endl ;
+
+
+    return 0;
+}
