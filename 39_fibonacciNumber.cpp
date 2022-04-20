0881ad5d8fd6bd3cae85a4ec5d8faf4d6ad1a0f0
Dhruv <dhruv.nitj@gmail.com>
2022-04-20 13:51:59 +0530

    fibonacci

---
+++ 39_fibonacciNumber.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cstdio>
+using namespace std ;
+
+int fibonacci(int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    else if (n ==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n-1)+fibonacci(n-2) ;
+    }    
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << "Enter the number: ";
+    int n;
+    cin >> n ;
+    cout << "Ans: " << fibonacci(n) << endl ;
+    return 0;
+}
