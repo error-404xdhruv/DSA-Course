7c4bdfc1bc349a72d7b96e1aafb92f4ab19520e1
Dhruv <dhruv.nitj@gmail.com>
2022-04-20 10:46:56 +0530

    NULL

---
+++ 37_functions1.cpp
@@ -0,0 +1,20 @@
+
+//  a function to determine whether a number is odd or even
+#include <iostream>
+#include <cstdio>
+using namespace std ;
+
+// returns 0 if odd and 1 if even
+bool isEven (int num)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int num ;
+    cin >> num ;
+
+
+    return 0;
+}
