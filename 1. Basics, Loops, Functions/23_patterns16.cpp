42a48a779f99a53ca7791bdf71faea4a07be91bd
Dhruv <dhruv.nitj@gmail.com>
2022-04-21 20:11:01 +0530

    apt21

--- 1. Basics, Loops, Functions/23_patterns16.cpp
+++ 1. Basics, Loops, Functions/23_patterns16.cpp
@@ -1,11 +1,5 @@
-
-// good level pattern question
 #include <iostream>
 #include <cstdio>
-#include <math.h>
-#include <cstring>
-#include <string>
-
 using namespace std ;
 
 int main(int argc, char const *argv[])
