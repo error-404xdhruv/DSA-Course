fb8cadf8b44086dc66872d5b7c3aadfaa8692a50
Dhruv <dhruv.nitj@gmail.com>
2022-04-23 13:38:33 +0530

    apr23

--- 2. Arrays/51_duplicateInArray.cpp
+++ 2. Arrays/51_duplicateInArray.cpp
@@ -5,9 +5,41 @@
 #include <iostream>
 using namespace std ;
 
+void doWork()
+{
+    int arr[100000];
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i] ;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < N; j++)
+        {
+            if (arr[i]==arr[j])
+            {
+                count++;
+            }
+        }
+        if (count>1)
+        {
+            cout << arr[i] << endl;
+            break;
+        }
+    }
+}
+
 int main()
 {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        doWork();
+    }
     
-
     return 0;
 }
