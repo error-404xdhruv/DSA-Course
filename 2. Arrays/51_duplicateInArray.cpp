5785b7990440508d7176efa444d69703bae2c521
Dhruv <dhruv.nitj@gmail.com>
2022-04-26 23:24:38 +0530

    apr26

--- 2. Arrays/51_duplicateInArray.cpp
+++ 2. Arrays/51_duplicateInArray.cpp
@@ -43,3 +43,24 @@ int main()
     
     return 0;
 }
+
+/*
+
+int findDuplicate(vector<int> &arr)
+{
+    int ans = 0;
+	
+// 	XORing all the elements
+	for (int i = 0; i < arr.size() ;i++)
+	{
+		ans = ans ^ arr[i];
+	}
+// 	XOR[1, N-1]
+	for(int i = 0; i < arr.size(); i++)
+	{
+		ans = ans ^ i;
+	}
+	return ans;
+}
+
+*/
