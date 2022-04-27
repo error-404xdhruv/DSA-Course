75d947fc7252bd388cac52e4327914508a61249a
Dhruv <dhruv.nitj@gmail.com>
2022-04-27 19:41:39 +0530

    apr27

--- 2. Arrays/54_tripletsWithGivenSum.cpp
+++ 2. Arrays/54_tripletsWithGivenSum.cpp
@@ -1,11 +1,47 @@
 
 // https://www.codingninjas.com/codestudio/problems/pair-sum_697295?leftPanelTab=1
-#include <iostream>
-using namespace std ;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void findSum(int array[], int n, int sum)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                if (array[i] + array[j] + array[k] == sum)
+                {
+                    cout << array[i] << " " << array[j] << " " << array[k] << endl;
+                    array[i] = INT_MIN;
+                    array[j] = INT_MIN;
+                    array[k] = INT_MIN;
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
-    
-    
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        int array[1000];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> array[j];
+        }
+        int sum;
+        cin >> sum;
+        findSum(array, n, sum);
+    }
+
+end:
     return 0;
 }
