17bfe8f69e52353fa4ed51436693d446288e8085
Dhruv <dhruv.nitj@gmail.com>
2022-04-30 20:28:38 +0530

    apr30

--- 3. Binary Search/4_LeetCode#852.cpp
+++ 3. Binary Search/4_LeetCode#852.cpp
@@ -1,5 +1,6 @@
 
 // https://leetcode.com/problems/peak-index-in-a-mountain-array/submissions/
+// lecture#13 - Luv Babbar
 
 class Solution {
 public:
@@ -7,7 +8,7 @@ public:
         
         
         int s = 0 , e = arr.size()-1 ;
-        while (s < e)
+        while (s < e)  // not used s <= e bcz we have to return the s when s = e ;
         {
             int mid = s + (e-s)/2 ;
             if (arr[mid]<arr[mid+1])
@@ -16,7 +17,7 @@ public:
             }
             else
             {
-                e = mid ;
+                e = mid ; // e= mid and not e= mid-1 bcz if we used the first case then are not going to get the ans any how.
             }
         }
         return s ;
