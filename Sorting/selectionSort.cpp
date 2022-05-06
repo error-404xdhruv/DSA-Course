0867adf98b87e16382100939e9aac2980f5e4a5c
Dhruv <dhruv.nitj@gmail.com>
2022-05-06 10:57:00 +0530

    may6

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -7,7 +7,7 @@ void doSort (int arr[], int n)
     for (int i = 0; i < n-1; i++)
     {
         min_idx = i ;
-        for (int j = i; j < n; j++)
+        for (int j = i+1; j < n; j++)
         {
             if (arr[j]<arr[min_idx])
             {
