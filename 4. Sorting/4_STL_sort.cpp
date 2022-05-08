e8612df9d8b6379f9ad25a8d794ece19486b118b
Dhruv <dhruv.nitj@gmail.com>
2022-05-08 23:42:22 +0530

    may8

---
+++ 4. Sorting/4_STL_sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // sort in increasing order
+    sort (arr, arr+5) ;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }cout << endl ;
+    
+    // sort in decreasing order
+    sort(arr, arr + 5, greater<int>());
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }cout << endl ;
+    return 0;
+}
