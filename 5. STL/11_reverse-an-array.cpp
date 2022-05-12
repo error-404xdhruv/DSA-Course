a70ce536c204533d1904b41cf69bf5d034a716d2
Dhruv <dhruv.nitj@gmail.com>
2022-05-12 13:39:33 +0530

    may12

---
+++ 5. STL/11_reverse-an-array.cpp
@@ -0,0 +1,31 @@
+
+// alrady did the same ques before using arrays, now we would do the same using vectors
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v1;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v1.push_back(a);
+    }
+    int s = 0, e = n - 1;
+    while (s < e)
+    {
+        swap(v1[s], v1[e]);
+        s++, e--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v1[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
