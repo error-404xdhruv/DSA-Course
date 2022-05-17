e65ce20601c7c7336447608a7fdb3ac02aac8dad
Dhruv <dhruv.nitj@gmail.com>
2022-05-17 23:18:29 +0530

    may17

--- 7. 2-D Arrays/5_rotate-image-leetcode.cpp
+++ 7. 2-D Arrays/5_rotate-image-leetcode.cpp
@@ -1,4 +1,29 @@
 
 // https://leetcode.com/problems/rotate-image/
+
+// must study method 2 given on the below link
 // https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/?ref=gcse
 
+#include <bits/stdc++.h>
+#define l long long
+using namespace std;
+
+void reverseMatrix (int matrix[][3], int n, int m)
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = n-1; i >= 0; i--)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl ;
+    }
+    
+}
+
+int main()
+{
+    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    reverseMatrix (matrix, 3, 3) ;
+    return 0;
+}
