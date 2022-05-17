16253fcaa3525f9c1e5bdc4925bcafd1c1b0fb8a
Dhruv <dhruv.nitj@gmail.com>
2022-05-17 23:29:03 +0530

    may17

--- 7. 2-D Arrays/6_binary-search-2D-matrix.cpp
+++ 7. 2-D Arrays/6_binary-search-2D-matrix.cpp
@@ -1,3 +1,33 @@
 
 // https://leetcode.com/problems/search-a-2d-matrix/
 
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int start = 0, end = rows * cols - 1, mid;
+        while (start <= end)
+        {
+            mid = start + (end - start) / 2;
+            if (matrix[mid / cols][mid % cols] == target)
+            {
+                return 1;
+            }
+            else if (matrix[mid / cols][mid % cols] < target)
+            {
+                start = mid + 1;
+            }
+            else if (matrix[mid / cols][mid % cols] > target)
+            {
+                end = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+};
