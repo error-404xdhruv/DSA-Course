8e686415df7b111b94192715456b1a4173a3d8c8
Dhruv <dhruv.nitj@gmail.com>
2022-05-18 17:58:42 +0530

    may18

--- 7. 2-D Arrays/7_search-2D-matrix-II-leetcode.cpp
+++ 7. 2-D Arrays/7_search-2D-matrix-II-leetcode.cpp
@@ -1,2 +1,34 @@
 
-// 
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int startRow = 0;
+        int startCol = col - 1;
+
+        while (startCol >= 0 && startRow < row)
+        {
+            int element = matrix[startRow][startCol];
+
+            if (element == target)
+            {
+                return 1;
+            }
+            else if (element < target)
+            {
+                startRow++;
+            }
+            else if (element > target)
+            {
+                startCol--;
+            }
+        }
+        return 0;
+    }
+};
