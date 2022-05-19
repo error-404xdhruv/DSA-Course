89842775e90413265929051dbae663243baaac58
Dhruv <dhruv.nitj@gmail.com>
2022-05-19 11:12:49 +0530

    may19

---
+++ 7. Greedy Algorithms/1_indian-coin-problem.cpp
@@ -0,0 +1,32 @@
+
+
+
+#include <bits/stdc++.h>
+#define l long long
+using namespace std ;
+
+int main()
+{
+    int n ;
+    cin >> n ;
+    vector <int> denom ;
+    int a ;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        denom.push_back(a) ;
+    }
+    sort (denom.begin(), denom.end(), greater<int>()) ;
+    int amount;
+    cin >> amount ;
+    int count =0 ;
+
+    for (int i = 0; i < n; i++)
+    {
+        count += amount / denom[i] ;
+        amount -= (amount/denom[i]) * denom[i] ;
+    }
+    cout << count <<endl ;
+
+    return 0;
+}
