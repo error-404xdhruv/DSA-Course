ee7a8e850048e0952bb06f177b304827ae229a67
Dhruv <dhruv.nitj@gmail.com>
2022-05-22 13:12:38 +0530

    may22

--- 7. Greedy Algorithms/4_expedi-expedition.cpp
+++ 7. Greedy Algorithms/4_expedi-expedition.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 #define l long long
-#define vl vector<long long> 
-#define vi vector<int> 
-using namespace std ;
+#define vl vector<long long>
+#define vi vector<int>
+#define vp vector<vector<int>>
+using namespace std;
 
 int main()
 {
-    l t ;
-    cin>> t ;
+    l t;
+    cin >> t;
     while (t--)
     {
         l n;
-        cin >> n ;
-        vector <vector<l>> v ;
+        cin >> n;
+        vector<vector<l>> v;
         for (l i = 0; i < n; i++)
         {
             for (l j = 0; j < 2; j++)
             {
-                cin >> v[i][j] ;
-            }    
+                cin >> v[i][j];
+            }
         }
         l L, P;
-        cin >> L >> P ;
+        cin >> L >> P;
+        
     }
-    
 
     return 0;
 }
