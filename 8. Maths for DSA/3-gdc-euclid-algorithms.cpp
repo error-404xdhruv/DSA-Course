14914af97f173d0709d1d040006e43ec64416e30
Dhruv <dhruv.nitj@gmail.com>
2022-05-25 18:16:11 +0530

    may25

--- 8. Maths for DSA/3-gdc-euclid-algorithms.cpp
+++ 8. Maths for DSA/3-gdc-euclid-algorithms.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 #define l long long
-#define vl vector<long long> 
-#define vi vector<int> 
-#define vp vector<vector<int>> 
-using namespace std ;
+#define vl vector<long long>
+#define vi vector<int>
+#define vp vector<vector<int>>
+using namespace std;
 
-int findGCD (int a, int b)
+int findGCD(int a, int b)
 {
-    if (a==0)
+    if (a == 0)
     {
-        return b ;
+        return b;
     }
     else
     {
-        findGCD(b%a, a) ;
+        findGCD(b % a, a);
     }
 }
 
 int main()
 {
-    int a, b ;
-    cin >> a >> b ;
-    cout << findGCD (a, b) << endl ;
+    int a, b;
+    cin >> a >> b;
+    cout << findGCD(a, b) << endl;
 
     return 0;
 }
