7c0073b1d467f653403f439b9d856e1ff4c3768c
Dhruv <dhruv.nitj@gmail.com>
2022-06-01 10:31:50 +0530

    jun1

--- 9. Strings/10-remove-adjacent-in-string.cpp
+++ 9. Strings/10-remove-adjacent-in-string.cpp
@@ -6,15 +6,6 @@ class Solution
 public:
     string removeDuplicates(string s)
     {
-        int i=0 ;
-        while (i<s.size())
-        {
-            if (s[i]==s[i+1])
-            {
-                s.erase(i, 2) ;
-                removeDuplicates(s) ;
-            }
-        }
-        return s ;        
+        
     }
 };
--- 9. Strings/11-string-compression.cpp
+++ 9. Strings/11-string-compression.cpp
@@ -2,3 +2,11 @@
 // https://leetcode.com/problems/string-compression/
 // time complexity : O(n)
 
+class Solution
+{
+public:
+    int compress(vector<char> &chars)
+    {
+        
+    }
+};
