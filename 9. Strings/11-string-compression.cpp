db077dc8d03c456f7de44c324c29b47f5fad2e53
Dhruv <dhruv.nitj@gmail.com>
2022-06-01 17:04:16 +0530

    jun1

--- 9. Strings/11-string-compression.cpp
+++ 9. Strings/11-string-compression.cpp
@@ -1,12 +1,45 @@
 
 // https://leetcode.com/problems/string-compression/
-// time complexity : O(n)
 
 class Solution
 {
 public:
     int compress(vector<char> &chars)
     {
-        
+        if (chars.size() < 2)
+        {
+            return chars.size();
+        }
+        string s;
+        int  n = chars.size() ;
+        for (int i = 0; i < n; )
+        {
+            int count =1 ;
+            for (int j = i+1; j < n && (chars[i]==chars[j]); j++)
+            {
+                if (chars[i]==chars[j])
+                {
+                    count++ ;
+                }
+            }
+            if (count==1)
+            {
+                s += chars[i] ;
+                i++ ;
+            }
+            else
+            {
+                s += chars[i] ;
+                s += to_string(count) ;
+                i += count ;
+            }
+        }
+        chars.clear() ;
+        for (int i = 0; i < s.length(); i++)
+        {
+            chars.push_back(s[i]) ;
+        }
+
+        return chars.size() ;
     }
 };
