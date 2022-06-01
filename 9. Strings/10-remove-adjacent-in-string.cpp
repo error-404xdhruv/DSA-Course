7d329ba54a75a65340635116f2a302b2a561687e
Dhruv <dhruv.nitj@gmail.com>
2022-06-01 11:53:23 +0530

    jun1

--- 9. Strings/10-remove-adjacent-in-string.cpp
+++ 9. Strings/10-remove-adjacent-in-string.cpp
@@ -1,11 +1,29 @@
 
 // https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
 class Solution
 {
 public:
     string removeDuplicates(string s)
     {
-        
+        int i = 0;
+        while (i < s.length())
+        {
+            if (s[i] == s[i + 1])
+            {
+                s.erase(i, 2);
+                if (i != 0)
+                {
+                    i--;
+                }
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return s;
     }
 };
