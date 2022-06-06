4a15e001040b8c0f2c3633369c18da7e73f7cc59
Dhruv <dhruv.nitj@gmail.com>
2022-06-06 11:31:58 +0530

    jun6

---
+++ 10. Recursion/4-print-1-to-n-backtracking.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long> 
+#define vi vector<int> 
+#define vp vector<vector<int>> 
+using namespace std ;
+
+void print (int i,int n)
+{
+    if (i < 1)
+    {
+        return ;
+    }
+    print (i-1, n) ;
+    cout << i << endl ;
+}
+
+int main()
+{
+    int n ;
+    cin >>  n;
+    print (n, n) ;
+
+    return 0;
+}
