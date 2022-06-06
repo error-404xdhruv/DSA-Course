06e1477460e05950afa717676e3067ea0788ab17
Dhruv <dhruv.nitj@gmail.com>
2022-06-06 17:51:11 +0530

    jun6

---
+++ 10. Recursion/11-nth-fibonacci.cpp
@@ -0,0 +1,27 @@
+
+// example of multiple recursion calls
+
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long>
+#define vi vector<int>
+#define vp vector<vector<int>>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
+
+    return 0;
+}
