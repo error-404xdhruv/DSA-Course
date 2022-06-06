d67c15aedb3a7fc211ecc7d08ad3829a3c47951d
Dhruv <dhruv.nitj@gmail.com>
2022-06-06 16:26:10 +0530

    jun6

--- 10. Recursion/9-reverse-an-array-recursion.cpp
+++ 10. Recursion/9-reverse-an-array-recursion.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/* #include <bits/stdc++.h>
 #define l long long
 #define vl vector<long long> 
 #define vi vector<int> 
@@ -26,5 +26,34 @@ int main()
 
     return 0;
 }
+ */
 
 // instaed of taking s and e we can also take s and n-s-1 
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long> 
+#define vi vector<int> 
+#define vp vector<vector<int>> 
+using namespace std ;
+
+void reverse (vi v,int i)
+{
+    if (i >= v.size()/2)
+    {
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i] << " " ;
+        }
+        return ;
+    }
+    swap (v[i], v[v.size()-i-1]) ;
+    reverse(v, i+1) ;
+}
+
+int main()
+{
+    vi v = {1, 2, 3, 4} ;
+    reverse (v, 0) ;
+
+    return 0;
+}
