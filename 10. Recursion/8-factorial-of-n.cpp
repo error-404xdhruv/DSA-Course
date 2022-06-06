213c84ed72667a59d1fbeefc8fe7ac57a37f9b2b
Dhruv <dhruv.nitj@gmail.com>
2022-06-06 15:13:43 +0530

    jun6

---
+++ 10. Recursion/8-factorial-of-n.cpp
@@ -0,0 +1,27 @@
+
+// using fuctional approach
+
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long> 
+#define vi vector<int> 
+#define vp vector<vector<int>> 
+using namespace std ;
+
+l factorial(int n)
+{
+    if (n==1)
+    {
+        return 1 ;
+    }
+    return n * factorial(n-1) ;    
+}
+
+int main()
+{
+    int n ;
+    cin >> n ;
+    cout << factorial(n) << endl ;
+
+    return 0;
+}
