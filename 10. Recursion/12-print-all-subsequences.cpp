676de9b48ab74cda25ec95f1c632bbba662ecb1d
Dhruv <dhruv.nitj@gmail.com>
2022-06-07 20:05:13 +0530

    jun7

---
+++ 10. Recursion/12-print-all-subsequences.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long>
+#define vi vector<int>
+#define vp vector<vector<int>>
+using namespace std;
+
+void printF (int start, int arr[] , vi v , int n)
+{
+    
+}
+
+int main()
+{
+    // lets take an array
+    int arr[] = {3, 1, 2};
+    int n = 3;
+    vi v ;
+    printF (0, arr, v, n) ;
+
+    return 0;
+}
