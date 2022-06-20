5029acb3c41b7b0ca0e51da6c2c55205bad2c8d2
Dhruv <dhruv.nitj@gmail.com>
2022-06-20 16:24:16 +0530

    jun20

--- 12. CPP - Pointers/3-intro-pointerArith.cpp
+++ 12. CPP - Pointers/3-intro-pointerArith.cpp
@@ -14,10 +14,10 @@ int main()
     // notice the output , we would get a difference of 4 in both the output , which is because we are changing the value stored at ptr and not at *ptr, value stored at ptr is some address and as we know that one int = 4 bits , so if ptr = 100 the ptr +1 would be 104
 
     // important property
-    
+
     int var = 6 ;
     int *ptr1 = &var ;
-    int *ptr2 = ptr1 ;
+    int *ptr2 = ptr1 ; // notice that we have not used & operator with ptr1
     cout << ++(*ptr2) << endl;
 
     return 0;
