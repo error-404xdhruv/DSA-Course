d8ce0629f42d97ef67731b1fde57cc45089ae3ef
Dhruv <dhruv.nitj@gmail.com>
2022-06-20 22:09:23 +0530

    jun20

---
+++ 12. CPP - Pointers/9-pointers-questions/ques01.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long> 
+#define vi vector<int> 
+#define v2 vector<vector<int>> 
+using namespace std ;
+
+int main()
+{
+    int(*p)[10][20];
+    cout << sizeof(*p) << endl ;
+    return 0;
+}
