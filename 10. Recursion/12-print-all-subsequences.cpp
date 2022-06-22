40872190e58b968196897e8ea79fccb9ee6c22bb
Dhruv <dhruv.nitj@gmail.com>
2022-06-22 21:03:21 +0530

    jun22

--- 10. Recursion/12-print-all-subsequences.cpp
+++ 10. Recursion/12-print-all-subsequences.cpp
@@ -1,22 +1,46 @@
 #include <bits/stdc++.h>
 #define l long long
-#define vl vector<long long>
-#define vi vector<int>
-#define vp vector<vector<int>>
-using namespace std;
+#define vl vector<long long> 
+#define vi vector<int> 
+#define v2 vector<vector<int>> 
+using namespace std ;
 
-void printF (int start, int arr[] , vi v , int n)
+void printSubsequences (vi arr , int index , vi &subarr, int n)
 {
+    if (index == n)
+    {
+        for (auto x: subarr)
+        {
+            cout << x << " ";
+        }
+        if (subarr.size()==0)
+        {
+            cout << "{ }" ;
+        }
+        cout << "\n" ;
+
+        return ;
+    }
+    else
+    {
+        subarr.push_back(arr[index+1]) ;
+    }
+    
     
 }
 
 int main()
 {
-    // lets take an array
-    int arr[] = {3, 1, 2};
-    int n = 3;
-    vi v ;
-    printF (0, arr, v, n) ;
+    int n ; cin>> n ;
+    vi v ; 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i] ;
+    }
+    
+    vi subarr ;
+    // according to the recursion tree, we would start from index 0
+    printSubsequences(v, 0, subarr, n) ;
 
     return 0;
 }
